aeba868c1ab29c7d28629167e9de0e848e5d0c5b
Nicholas <nicholas0639@protonmail.ch>
2026-04-28 14:42:50 -0700

    Made progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.gamifiedworkouttracker"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.gamifiedworkouttracker"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
