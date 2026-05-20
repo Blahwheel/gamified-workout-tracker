b66145cbcfe4b45218291766f56c5c291af29c2e
Nicholas <nicholas0639@protonmail.ch>
2026-05-19 17:29:54 -0700

    Added images to project; added constraints to images

--- app/src/main/java/com/example/gamifiedworkouttracker/LiftingScreen.java
+++ app/src/main/java/com/example/gamifiedworkouttracker/LiftingScreen.java
@@ -1,6 +1,7 @@
 package com.example.gamifiedworkouttracker;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LiftingScreen extends AppCompatActivity {
+
+    // fields
+
+    ImageView exerciseImage;
+    WorkoutTracker workoutTracker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,34 @@ public class LiftingScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // our stuff
+        workoutTracker = WorkoutTracker.getInstance();
+        initPicture();
+        gameLoop();
+    }
+
+    private void initPicture() {
+        exerciseImage = findViewById(R.id.imageView);
+        String exerciseName = workoutTracker.getCurrentExercise();
+
+        switch (exerciseName) {
+            case "bench_press":
+                exerciseImage.setImageResource(R.drawable.bench);
+                break;
+            case "shoulder_press":
+                exerciseImage.setImageResource(R.drawable.shoulder_press);
+                break;
+            case "cable_row":
+                exerciseImage.setImageResource(R.drawable.cable_row);
+                break;
+            default:
+                exerciseImage.setImageResource(R.drawable.gym);
+                break;
+        }
+    }
+    private void gameLoop(){
+        
     }
 
 
